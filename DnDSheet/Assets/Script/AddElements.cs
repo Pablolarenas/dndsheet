a9a6da5c0aa2a141d94947735f34004a0b46d48a
Pollo <pabloalarenas@gmail.com>
2018-12-16 19:55:04 +0000

    multiplicacion

--- DnDSheet/Assets/Script/AddElements.cs
+++ DnDSheet/Assets/Script/AddElements.cs
@@ -10,14 +10,31 @@ public class AddElements : MonoBehaviour
     [SerializeField] private InputField result;
     [Header("-----------------------------")]
     [SerializeField] private Toggle toggleToListen;
+    [Header("-----------------------------")]
     private UiManager uiManager;
+    public enum TypeOfCalculation { Suma, Multiplicacion };
+    public TypeOfCalculation typeOfCalculation = TypeOfCalculation.Suma;
+    private delegate void CalculationMethod();
+    private CalculationMethod calculationMethod;
 
     private void Awake()
     {
         uiManager = FindObjectOfType<UiManager>();
+
+        switch (typeOfCalculation)
+        {
+            case TypeOfCalculation.Suma:
+                calculationMethod = Suma;
+                break;
+            case TypeOfCalculation.Multiplicacion:
+                calculationMethod = Multiplicacion;
+                break;
+            default:
+                break;
+        }
     }
 
-    public void UpdateValue()
+    private void Suma()
     {
         int totalValue = 0;
         foreach (InputField item in ListOfElementsToAdd)
@@ -47,5 +64,42 @@ public class AddElements : MonoBehaviour
         }
 
         result.text = value;
+    }
+
+    private void Multiplicacion()
+    {
+        int totalValue = 0;
+        foreach (InputField item in ListOfElementsToAdd)
+        {
+            if (item.text == string.Empty) continue;
+
+            if (item.text[0] == '-')
+            {
+                totalValue *= -int.Parse(Regex.Match(item.text, @"\d+").Value);
+            }
+            else
+            {
+                totalValue *= int.Parse(Regex.Match(item.text, @"\d+").Value);
+            }
+        }
+
+        if (toggleToListen != null && toggleToListen.isOn)
+        {
+            totalValue += uiManager.ProfValue;
+        }
+
+        string value = totalValue.ToString();
+
+        if (totalValue > 0)
+        {
+            value = string.Concat("+", value);
+        }
+
+        result.text = value;
+    }
+
+    public void UpdateValue()
+    {
+        calculationMethod();
     }
 }
