f349891681db48bfbcc19bacfb64d34cd2c9e119
PedroQuijadaLeyton <pedro@quiley.com>
2018-12-16 12:05:49 +0000

    Added navigation script caca

---
+++ DnDSheet/Assets/Script/NavButtonTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class NavButtonTrigger : MonoBehaviour, IPointerClickHandler
+{
+    public UiState whenPressedChangeTo;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        FindObjectOfType<UiManager>().SetNewUiState(whenPressedChangeTo);
+    }
+}
---
+++ DnDSheet/Assets/Script/UiManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+
+[Serializable]
+public class UiElement
+{
+    public string Name;
+    public UiState UiState;
+    public GameObject UiObject;
+}
+
+public enum UiState { Character, Stats, Combat, Inventory, Spells, Notes }
+
+public class UiManager : MonoBehaviour
+{
+    public List<UiElement> ListOfUiElement;
+    private UiState currentUiState = UiState.Character; //INITIAL STATE
+    public UiState CurrentUiState
+    {
+        get { return currentUiState; }
+        set
+        {
+            if (currentUiState != value)
+            {
+                CheckUiState(currentUiState, value);
+            }
+            currentUiState = value;
+        }
+    }
+
+    private void CheckUiState(UiState from, UiState to)
+    {
+        ListOfUiElement.Where(type => type.UiState == from).SingleOrDefault().UiObject.SetActive(false);
+        ListOfUiElement.Where(type => type.UiState == to).SingleOrDefault().UiObject.SetActive(true);
+    }
+
+    public void SetNewUiState(UiState newUiState)
+    {
+        CurrentUiState = newUiState;
+    }
+}
