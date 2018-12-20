abeaf971db6c95267d73ab0a3c50b4ea5ae478ff
PedroQuijadaLeyton <pedro@quiley.com>
2018-12-20 22:30:27 +0000

    added collapsible

--- DnDSheet/Assets/Script/Collapsible.cs
+++ DnDSheet/Assets/Script/Collapsible.cs
@@ -8,11 +8,16 @@ public class Collapsible : MonoBehaviour
     [SerializeField] private GameObject contentToSpawn;
     private int currentIndex;
     private List<GameObject> extraElements;
-    private bool isActive = true;
+    private bool isExpanded = false;
     
     private void Awake()
     {
         extraElements = new List<GameObject>();
+        GetCurrentChildId();
+    }
+
+    private void GetCurrentChildId()
+    {
         for (int i = 0; i < transform.parent.childCount; i++)
         {
             if (transform.parent.GetChild(i).name == gameObject.name)
@@ -22,27 +27,30 @@ public class Collapsible : MonoBehaviour
             }
         }
     }
-    public void Collapse()
+
+    public void Collapse(bool expand = false)
     {
-        isActive = !isActive;
+        isExpanded = !expand ? !isExpanded : true;
 
         if (extraElements.Count != 0)
         {
             foreach (GameObject item in extraElements)
             {
-                item.SetActive(isActive);
+                item.SetActive(isExpanded);
             }
         }
-        collapsibleContent.SetActive(isActive);
+        collapsibleContent.SetActive(isExpanded);
         
     }
 
     public void SpawnElement()
     {
+        GetCurrentChildId();
         Transform instance = Instantiate(contentToSpawn).transform;
         instance.SetParent(transform.parent, false);
         instance.SetSiblingIndex(currentIndex + 1);
         extraElements.Add(instance.gameObject);
+        Collapse(true);
     }
 
 }
